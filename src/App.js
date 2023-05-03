6a8c4d243e43637013a62dedda35528b5b6a1165
Radhika <rvramayra1998@gmail.com>
2023-05-03 15:52:17 +0530

    2sxs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      {/* <Route path='/react_project' element={<Home/>}/> */}
+      <Route path='/react_project' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/profile' element={<Profile/>}/>
     </Routes>
